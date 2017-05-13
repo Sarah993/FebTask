a7cdf83eaf5b6e1a551a144d4fcd9092f4cb7872
sajedah <yousef.sajedah93@gmail.com>
2017-05-13 22:33:39 +0300

    deploying

--- app/app.js
+++ app/app.js
@@ -2,7 +2,45 @@ angular.module('feb',[])
 
 
 .controller('febController', function ($scope) {
-	
+	$scope.fib=[];
+	$scope.index = 0 ; 
+	$scope.rand = 0 ;
+	$scope.res = 0.00;
 
+
+
+	$scope.generate = function(){
+		$scope.fib[0] = 0;
+ 	$scope.fib[1] = 1;
+ 	for(i=2; i<=n; i++){
+ 		$scope.fib[i] = $scope.fib[i-2] + $scope.fib[i-1];
+ 	}
+ 	return $scope.fib;
+	}
+
+
+	$scope.rand = function(){
+		 $scope.rand = $scope.fib[Math.floor(Math.random() * $scope.fib.length)];
+		return rand
+	}
+
+
+	$scope.increase= function(){
+	$scope.index = fib.indexOf(rand)
+ 	return $scope.fib[$scope.index+1]
+
+	}
+
+	$scope.decrease = function(){
+		$scope.index = $scope.fib.indexOf($scope.rand)
+ 	return $scope.fib[$scpope.index-1]
+
+	}	
+
+	$scope.clear = function(){
+		$scope.fib = [] ;
+	$scope.rand = 0 ;
+	$scope.index = 0 ;
+	}
   });
 
