0e1224b8080f50a4a579b94ffe4f568166087a39
sajedah <yousef.sajedah93@gmail.com>
2017-05-14 14:41:01 +0300

    editing code

--- app/app.js
+++ app/app.js
@@ -6,26 +6,22 @@ angular.module('feb',[])
 	$scope.index = 0 ; 
 	$scope.rand = 0 ;
 	$scope.res = 0.00;
-	$scope.textValue = 0 ;
-	var that = this;
+	$scope.value = {}
+ 
 
- that.getValue = function () {
-      $scope.textValue = that.searchText;
-    };
-
-
+   
 
 	$scope.generate = function(){
-    $scope.textValue = that.searchText;
+   
 	$scope.fib[0] = 0;
  	$scope.fib[1] = 1;
- 	for(i=2; i<=$scope.textValue; i++){
+ 	for(i=2; i<=$scope.value.val; i++){
  		$scope.fib[i] = $scope.fib[i-2] + $scope.fib[i-1];
  	}
 	}
 
 
-	$scope.rand = function(){
+	$scope.random = function(){
 		 $scope.rand = $scope.fib[Math.floor(Math.random() * $scope.fib.length)];
 		 $scope.res = $scope.rand;
 	}
@@ -34,25 +30,32 @@ angular.module('feb',[])
 	$scope.calculate = function(){
 		$scope.generate();
 		console.log($scope.fib)
-		$scope.rand();
+		$scope.random();
 	}
 
 	$scope.increase= function(){
-	$scope.index = $scope.fib.indexOf($scope.rand)
+	$scope.index = $scope.fib.indexOf($scope.res)
  	$scope.res =  $scope.fib[$scope.index+1]
+ 	console.log($scope.value)
 
 	}
 
 	$scope.decrease = function(){
-		$scope.index = $scope.fib.indexOf($scope.rand)
- 	return $scope.fib[$scope.index-1]
+		$scope.index = $scope.fib.indexOf($scope.res)
+		 	$scope.res =  $scope.fib[$scope.index-1]
+
 
 	}	
 
 	$scope.clear = function(){
 	$scope.fib = [] ;
-	$scope.rand = 0 ;
+	$scope.res = 0;
 	$scope.index = 0 ;
+	$scope.value.val = ''
+	
+
+
+
 	}
   });
 
